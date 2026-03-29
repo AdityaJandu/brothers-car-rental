bb24ed0bfd44deb1dee4210935e25c5705640a63
AdityaJandu <janduaditay@gmail.com>
2026-03-29 16:13:49 +0530

    feat: initial commit

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,14 @@
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 
+const connectionString = process.env.DATABASE_URL
 
-const client = postgres(process.env.DATABASE_URL!)
-export const db = drizzle({ client });
+if (!connectionString) {
+    throw new Error("DATABASE_URL is not defined in environment variables");
+}
+
+const client = postgres(connectionString);
+export const db = drizzle(client);
 
 
 
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -96,10 +96,6 @@ export const account = pgTable(
         accessTokenExpiresAt: timestamp("access_token_expires_at"),
         refreshTokenExpiresAt: timestamp("refresh_token_expires_at"),
         scope: text("scope"),
-
-        // NOTE: password is hashed by Better Auth internally
-        password: text("password"),
-
         createdAt: timestamp("created_at").defaultNow().notNull(),
 
         updatedAt: timestamp("updated_at")
