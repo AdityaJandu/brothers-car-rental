2ec5bbe4b77703fae569dee394ffa0e6d20c6981
AdityaJandu <janduaditay@gmail.com>
2026-04-17 00:50:53 +0530

    refractor: removed unused file and updated code

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,18 +1,10 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/inngest/client";
 import {
-    sendConfirmationEmail,
-    expirePendingBooking,
-    sendBookingReminder,
-    sendStatusChangeEmailFn,
-} from "@/inngest/functions";
+    allFunctions
+} from "@/inngest/index";
 
 export const { GET, POST, PUT } = serve({
     client: inngest,
-    functions: [
-        sendConfirmationEmail,
-        expirePendingBooking,
-        sendBookingReminder,
-        sendStatusChangeEmailFn,
-    ],
+    functions: allFunctions,
 });
